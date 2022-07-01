fe21522452d3961de42c1174d6cae9ae2085ed64
Adalid <adalidirvin@gmail.com>
2022-06-30 21:21:24 -0400

    Base del proyecto

---
+++ ExamenProgII-SIM/MainForm.vb
@@ -0,0 +1,18 @@
+﻿'
+' Created by SharpDevelop.
+' User: Adalid
+' Date: 30/6/2022
+' Time: 20:44
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Public Partial Class MainForm
+	Public Sub New()
+		' The Me.InitializeComponent call is required for Windows Forms designer support.
+		Me.InitializeComponent()
+		
+		'
+		' TODO : Add constructor code after InitializeComponents
+		'
+	End Sub
+End Class
